939d96ef7246e740f5bab3fd4bfcc97b0da6a793
jciafardo <ciafardoj@gmail.com>
2023-12-27 14:18:31 -0500

    final done

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,7 @@
+"""
+'File for running the server'.
+"""
+
 from flask import Flask, render_template, request
 from emotion_detection.emotion_detection import emotion_detector
 
@@ -5,27 +9,28 @@ app = Flask("Emotion Detection")
 
 @app.route('/emotionDetector')
 def sent_analyzer():
-    
+    """
+    Sends statement to be analyzed.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
-    dominant_emotion = response['Dominant Emotion']
-    if dominant_emotion == None:
-        return("Invalid text please try again")
-    emotion_scores = ', '.join(f"'{emotion}': {score}" for emotion, score in response.items() if emotion != 'Dominant Emotion')
-    
-    output_string = f"For the given statement, the system response is {emotion_scores}. The dominant emotion is {dominant_emotion}."
+    dominant_emotion = response.get('Dominant Emotion')
+
+    if dominant_emotion is None:
+        return "Invalid text. Please try again"
+
+    emotion_scores = ', '.join(f"'{emotion}': {score}" for emotion, score in response.items() \
+    if emotion != 'Dominant Emotion')
+    output_string = f"For the given statement, the system response is {emotion_scores}. \
+    The dominant emotion is {dominant_emotion}."
     return output_string
-    
-
-
 
 @app.route('/')
 def render_index_page():
+    """
+    Render our main page.
+    """
     return render_template("index.html")
 
-@app.route('/test')
-def Test():
-    return render_template("index.html")
-
-app.run(host="0.0.0.0", port=5006)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
